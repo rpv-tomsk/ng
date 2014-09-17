ad3ed774faf0fcf58f7059548790acda00d85832
Pavel Rochnyack <pavel2000@ngs.ru>
2014-09-17 17:24:13 +0000

    Allow uppercase letters in is_valid_email
    git-svn-id: http://svn-proxy/repos/ng/branches/ng6@2144 5d2ed3fb-774c-4d2d-a2ad-e8474e2e3017

--- perl/lib/NSecure.pm
+++ perl/lib/NSecure.pm
@@ -57,7 +57,7 @@ sub is_valid_id {
 sub is_valid_email {
 	my $email = shift;
 	$email = "" if !defined $email;
-	return $email =~ /^(?:[_a-z0-9]+(?:[-.]?[_a-z0-9]+)*@[a-z0-9][a-z0-9\-]*(?:\.?[a-z0-9][a-z0-9\-]*)?\.[a-z]{2,5})$/i;
+	return $email =~ /^(?:[_A-Za-z0-9]+(?:[-.]?[_A-Za-z0-9]+)*@[A-Za-z0-9][A-Za-z0-9\-]*(?:\.?[A-Za-z0-9][A-Za-z0-9\-]*)?\.[A-Za-z]{2,5})$/i;
 }
 
 sub is_valid_link {
