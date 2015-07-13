806a75921c5a758169579414f39f1a0286c23b42
Pavel Rochnyack <pavel2000@ngs.ru>
2015-07-13 16:16:59 +0000

    * Fixed codestyle
    git-svn-id: http://svn-proxy/repos/ng/branches/ng6@2373 5d2ed3fb-774c-4d2d-a2ad-e8474e2e3017

--- perl/lib/NG/Module/List/Row.pm
+++ perl/lib/NG/Module/List/Row.pm
@@ -8,24 +8,24 @@ use vars qw(@ISA);
 BEGIN {};
 
 sub new {
-	my $class = shift;
-	my $obj = {};
-	bless $obj, $class;
-	$obj->init(@_);
-	return $obj;
+    my $class = shift;
+    my $obj = {};
+    bless $obj, $class;
+    $obj->init(@_);
+    return $obj;
 };
 
 sub init {
-	my ($self,$params) = (shift,shift);
+    my ($self,$params) = (shift,shift);
     
-	$self->{ROW}      = $params->{DATA};       # Хеш данных
-	$self->{_listObj} = $params->{LISTOBJ};
+    $self->{ROW}      = $params->{DATA};       # Хеш данных
+    $self->{_listObj} = $params->{LISTOBJ};
     $self->{_index}   = $params->{INDEX};      # Индекс строки в выборке
     my $id            = $params->{ID};         # ID записи строки
     
-	$self->{ROW_LINKS} = [];     #Массив ссылок операций со строкой
-	$self->{COLUMNS} = [];       #Массив столбцов рядка
-	$self->{HFIELDS} = {};       #Хеш объектов NG::Field
+    $self->{ROW_LINKS} = [];     #Массив ссылок операций со строкой
+    $self->{COLUMNS} = [];       #Массив столбцов рядка
+    $self->{HFIELDS} = {};       #Хеш объектов NG::Field
     
     $self->{KEY_VALUE} = "_$id";  #Используется в шаблоне 
     $self->{ID_VALUE} = $id;      #В том числе для шаблона
@@ -46,8 +46,8 @@ sub init {
 #Метод, используемый в унаследованных классах
  
 sub _addColumn {
-	my ($self, $field) = (shift,shift);
-	return if ($field->{TYPE} eq "posorder");
+    my ($self, $field) = (shift,shift);
+    return if ($field->{TYPE} eq "posorder");
     
     $self->{HFIELDS}->{uc($field->{FIELD})} = $field;
     
@@ -68,55 +68,55 @@ sub _addColumn {
 
 #Метод, используемый в унаследованных классах
 sub pushLink {
-	my $self = shift;
-	my $tmplink = shift;
-	my $link = undef;
-	%{$link} = %{$tmplink};
-	
-	while ($link->{URL} =~ /\{(.+?)\}/i) {
-		my $value = $self->getParam($1);
-		$link->{URL} =~ s/\{(.+?)\}/$value/i;
-	};
-	
-	if (($link->{URL} =~ /^\?/ || $link->{URL} !~ /^\//) && $link->{URL} !~ /^[a-zA-Z0-9]+\:\/\//) {
-		$link->{URL} = $self->list()->getBaseURL().$self->list()->getSubURL().$link->{URL};
-	};
-	
+    my $self = shift;
+    my $tmplink = shift;
+    my $link = undef;
+    %{$link} = %{$tmplink};
+    
+    while ($link->{URL} =~ /\{(.+?)\}/i) {
+        my $value = $self->getParam($1);
+        $link->{URL} =~ s/\{(.+?)\}/$value/i;
+    };
+    
+    if (($link->{URL} =~ /^\?/ || $link->{URL} !~ /^\//) && $link->{URL} !~ /^[a-zA-Z0-9]+\:\/\//) {
+        $link->{URL} = $self->list()->getBaseURL().$self->list()->getSubURL().$link->{URL};
+    };
+    
     $link->{URL} = getURLWithParams($link->{URL},"ref=".$self->list()->buildRefCurrentUrl());
-	if ($link->{AJAX}) {
-		$link->{AJAX_URL} = getURLWithParams($link->{URL},"_ajax=1","rand=".int(rand(10000)));
-	};
-	push @{$self->{ROW_LINKS}}, $link;
+    if ($link->{AJAX}) {
+        $link->{AJAX_URL} = getURLWithParams($link->{URL},"_ajax=1","rand=".int(rand(10000)));
+    };
+    push @{$self->{ROW_LINKS}}, $link;
 }; 
 
 #Метод, перекрываемый в унаследованных классах
 sub buildLinks {
-	my $self = shift;
-	my $deletelink = $self->list()->getDeleteLink();
-	my $extralinks = $self->list()->getExtraLinks();
-	foreach my $link (@{$extralinks}) {
-		$self->pushLink($link);
-	};
-	$self->pushLink($deletelink) if ($deletelink);
+    my $self = shift;
+    my $deletelink = $self->list()->getDeleteLink();
+    my $extralinks = $self->list()->getExtraLinks();
+    foreach my $link (@{$extralinks}) {
+        $self->pushLink($link);
+    };
+    $self->pushLink($deletelink) if ($deletelink);
 };
 
 #Методы, используемые в унаследованных классах
 sub getParam {
-	my $self = shift;
-	my $param = shift;
-	return $self->{ROW}->{$param};
+    my $self = shift;
+    my $param = shift;
+    return $self->{ROW}->{$param};
 };
 
 sub getField {
-	my $self = shift;
-	my $fieldname = shift;
-	return $self->{HFIELDS}->{uc($fieldname)};
+    my $self = shift;
+    my $fieldname = shift;
+    return $self->{HFIELDS}->{uc($fieldname)};
 };
 
 sub getValue {
-	my $self = shift;
-	my $fieldname = shift;
-	return $self->{HFIELDS}->{uc($fieldname)}->value();
+    my $self = shift;
+    my $fieldname = shift;
+    return $self->{HFIELDS}->{uc($fieldname)}->value();
 };
 
 sub row {
@@ -125,28 +125,28 @@ sub row {
 };
 
 sub list {
-	my $self = shift;
-	return $self->{_listObj};
+    my $self = shift;
+    return $self->{_listObj};
 };
 
 sub db {
-	my $self = shift;
-	return $self->{_listObj}->db();
+    my $self = shift;
+    return $self->{_listObj}->db();
 };
 
 sub getDocRoot {
-	my $self = shift;
-	return $self->{_listObj}->getDocRoot();
+    my $self = shift;
+    return $self->{_listObj}->getDocRoot();
 };
 
 sub getSiteRoot {
-	my $self = shift;
-	return $self->{_listObj}->getSiteRoot();
+    my $self = shift;
+    return $self->{_listObj}->getSiteRoot();
 };
 
 sub highlight {
-	my $self = shift;
-	$self->{HIGHLIGHT} = 1;
+    my $self = shift;
+    $self->{HIGHLIGHT} = 1;
 };
 
 return 1;
