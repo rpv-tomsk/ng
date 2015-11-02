rpv-tomsk/ng

commit dc8aaa493af5d7397512d90e44d8aa15f23908f1

* Reworked admin-side CSS and look-n-feel
git-svn-id: http://svn-proxy/repos/ng/branches/ng6@2435 5d2ed3fb-774c-4d2d-a2ad-e8474e2e3017

perl/lib/NG/Module/List.pm
@@ -66,7 +66,6 @@ sub init {
     $self->{_shlistPageParam}="";
     $self->{_shlistWhere}=[];
     $self->{_shlistSearch}=[];
-    $self->{_shlistFields}=undef;
     
     $self->{_filters} = []; # Список фильтров списка
     $self->{_multiActions} = undef;  #Список возможных действий пользователя над группой записей.
@@ -79,6 +78,7 @@ sub init {
     $self->{_has_move_link} = 0;  ### Живет потому что есть метод disableMoveLink
     $self->{_hide_move_link} = 0;
     $self->{_posField} = undef;   ### Поле типа posorder
+    $self->{_showCounter} = 0;     ### Выводить номер строки БД в списке
     $self->{_delete_link_name} = "Удалить";
     
     $self->{_listtemplate} = "admin-side/common/universallist.tmpl";
@@ -159,14 +159,24 @@ sub buildList {
     # Заполняем описания полей списка описаниями из основного массива
     my $listfields = $self->_getIntersectFields($self->{_listfields}) or return $self->showError("buildList(): Ошибка вызова _getIntersectFields()");
     
+    my $listFObjs = [];
+    foreach my $f (@$listfields) {
+        if ($f->{FIELD} eq '_counter_') {
+            $self->{_showCounter} = $f;
+            next;
+        };
+        my $fObj = NG::Field->new($f,$self);
+        push @$listFObjs, $fObj;
+    };
+    
     $self->processFKFields() or return $self->showError("buildList(): Ошибка вызова processFKFields()"); # Если в описании таблицы есть FK, учитываем их в ссылках и SQL
     $self->processFilters() or return $self->showError("buildList(): Ошибка вызова processFilters()");
-    $self->processSorting($listfields) or return $self->showError("buildList(): Ошибка вызова processSorting()");
+    $self->processSorting() or return $self->showError("buildList(): Ошибка вызова processSorting()");
     $self->setPagesParam();
-    $self->highlightSortedColumns($listfields) or return $self->showError("buildList(): Ошибка вызова highlightSortedColumns()");
     
-    my @columns = $self->getListColumns($listfields);  # Также выставляет список полей для getListSQLFields
-    my $headersCnt = scalar @columns;
+    my @headers = $self->getListHeaders($listFObjs);
+    $self->highlightSortedColumns(\@headers) or return $self->showError("buildList(): Ошибка вызова highlightSortedColumns()");
+    my $headersCnt = scalar @headers;
     
     $headersCnt++ if $self->{_multiActions};
     
@@ -251,7 +261,7 @@ sub buildList {
     my $dblist = NG::DBlist->new(
         db     => $self->db(),
         table  => $self->getListSQLTable(),
-        fields => $self->getListSQLFields(),
+        fields => $self->getListSQLFields($listFObjs),
         where  => $self->getListSQLWhere(),
         order  => $self->getListSQLOrder(),
         pagename=>"page",
@@ -302,9 +312,7 @@ sub buildList {
     
     my $template = $self->template() || return $self->error("NG::Module::List::buildList(): Template not opened");
     $template->param(
-        HEADERS   => \@columns, #TODO: this can be $self->getListHeaders(),
-        #HEADERS_CNT => $headersCnt,
-        HEADERS_CNT_M1 => scalar(@columns)-1,  #Для чего это? (rpv,2014-11-27)
+        HEADERS   => \@headers,
         PAGES     => $pages,
         DATA      => \@arraydata,
         FILTERS    => $self->getListFilters(),
@@ -414,7 +422,7 @@ sub processForm {
     if ($action eq "insf" || $action eq "updf" || (!$is_ajax && $action eq "formaction")) {
         #Построение ссылки возврата после добавления записи
         $self->processFilters() or return $self->showError("processForm(): Ошибка вызова processFilters()");
-        $self->processSorting([]) or return $self->showError("processForm(): Ошибка вызова processSorting()");
+        $self->processSorting() or return $self->showError("processForm(): Ошибка вызова processSorting()");
 
         $formurl = getURLWithParams($formurl,
             $self->getFilterParam(), # На текущий момент не используется в формирования ссылки перехода на добавленную запись
@@ -1191,7 +1199,7 @@ sub _maDeleteRecords {
     return $self->outputJSON({status=>'ok'});
 };
 
- sub afterMove { my ($self, $id, $moveDir) = @_; }
+sub afterMove { my ($self, $id, $moveDir) = @_; }
 
 sub Move {
     my $self = shift;
@@ -1213,8 +1221,7 @@ sub Move {
     my $posField = $self->getPosField() or return $self->showError("Move(): поле позиционирования не найдено");   
     $self->processFKFields() or return $self->showError("Move(): Ошибка вызова processFKFields()"); # Если в описании таблицы есть FK, учитываем их в ссылках и SQL
     $self->processFilters()  or return $self->showError("Move(): Ошибка вызова processFilters()");
-    #TODO: вызов processSorting с [] - это хак.
-    $self->processSorting([])  or return $self->showError("Move(): Ошибка вызова processSorting()");
+    $self->processSorting()  or return $self->showError("Move(): Ошибка вызова processSorting()");
     
     die "Internal error:_shlistActiveOrder not defined." unless $self->{_shlistActiveOrder};
     my $found   = $self->{_shlistActiveOrder}->{ORDER};
@@ -1230,6 +1237,7 @@ sub Move {
     };
 
     my $posFieldName = $posField->{FIELD};
+    return $self->error("Некорректная ссылка перемещения") unless $posFieldName eq $found->{FIELD};
     my $idFieldName  = $self->{_idname};
     
     my @values = $self->getListSQLValues();
@@ -1447,7 +1455,7 @@ sub getBlockIndex {   #Вызывается из NG::PageModule->updateSearchIndex() на созд
             else {
               return $self->error("Поле ".$field->{FIELD}." не является fkparent или id. Значение ключа из суффикса не может быть игнорировано.А еще SUFFIXFIELD какой-то не понятный :).");
             };
-         };
+        };
         
         #Добавляем в список полей поля, описанные в суффиксе.
         #Их значения могут пригодиться в обработчиках, используются при обратном формировании суффикса.
@@ -2067,21 +2075,20 @@ sub getListFilters {
 
 sub processSorting {
     my $self = shift;
-    my $listfields = shift or confess "processSorting(): incorrect usage";
     
     my $q = $self->q();
 
     my $posField = $self->getPosField();
     if ($posField) {
-        push @{$listfields}, $posField;
         my $order = {};
         $order->{FIELD} = $posField->{FIELD};
         $order->{DEFAULTBY} = "DESC" if $posField->{REVERSE};
         unshift @{$self->{_orders}}, $order;
     };
+    
+    return NG::Block::M_OK unless scalar @{$self->{_orders}};
 
-    my $orderfield = "";
-    my $dir;
+    my ($orderfield,$dir) = ('','');
     if ($q->url_param('asc')) {
         $orderfield = $q->url_param('asc');
         $dir = "ASC";
@@ -2090,9 +2097,8 @@ sub processSorting {
         $orderfield = $q->url_param('desc');
         $dir = "DESC";
     };
-    return NG::Block::M_OK unless scalar @{$self->{_orders}};
-    my $default=undef;
-    my $found = undef;
+    
+    my ($found,$default) = (undef,undef);
     foreach my $iorder (@{$self->{_orders}}) {
         if ($iorder->{FIELD} eq $orderfield){
             $found = $iorder;
@@ -2137,41 +2143,38 @@ sub getFilterParam {
 };
 
 #Методы построения списка
-sub getListColumns {
+sub getListHeaders {
     my $self = shift;
-    my $listfields = shift or die "getListColumns(): incorrect usage";
+    my $listFObjs = shift or die "getListHeaders(): incorrect usage";
     
-    # Составляем строку-список запрашиваемых полей. При этом контролируем вхождение туда ключевого поля.
-    my @columns=();
-    my $fields = "";
-    my $idfound = 0;
-    foreach my $field (@{$listfields}) {
-        $idfound = 1 if ($field->{TYPE} eq "id");
-        push @columns,$field unless ($field->{TYPE} eq "hidden");  # Не включаем в список на отображение
-        next if ($field->{FIELD} eq "_counter_");                  ## не включаем в список на выборку
-        next if ($field->{'IS_FAKEFIELD'}); #не включаем в выборку вычисляемые поля
-        $fields .= ",".$field->{FIELD};
-        $field->{IS_POSORDER} = 1 if ($field->{TYPE} eq "posorder");
-        $field->{ORDER} = getURLWithParams($field->{ORDER},$self->getFilterParam(),$self->getFKParam()) if ($field->{ORDER});
+    my @headers = ();
+    foreach my $fObj (@{$listFObjs}) {
+        next if $fObj->{TYPE} eq "posorder";
+        #
+        my $headerCell = $fObj->getListHeaderCell();
+        $headerCell->{FIELD} = $fObj->{FIELD};
+        push @headers, $headerCell;
     };
-    
-    if ($idfound == 0) {
-        $fields .= ",".$self->{_idname}; # Если ключевое поле не найдено
-        push @{$listfields}, {
-            TYPE=>"hidden",
-            FIELD=>$self->{_idname},
-            NAME=>$self->{_idname},
+    my $posOrder = $self->getPosField();
+    if ($posOrder) {
+        my $headerCell = {
+            CLASS => 'posorder',
+            NAME  => $posOrder->{NAME} || 'Позиция',
+            FIELD => $posOrder->{FIELD},
+        };
+        push @headers, $headerCell;
+    };
+    if ($self->{_showCounter}) {
+        unshift @headers, {
+            NAME => '№',
         };
     };
-    $fields =~ s/^,//;
-    $self->{_shlistFields} = $fields;
-    return @columns;
+    return @headers;
 };
 
-sub highlightSortedColumns {
-    my $self = shift;
-    my $listfields = shift or confess "highlightSortedColumns(): incorrect usage";
-    #Подсветка столбцов, по которым можно делать сортировку
+sub highlightSortedColumns { #Подсветка столбцов, по которым можно делать сортировку
+    my ($self,$columns) = (shift,shift);
+    $columns or confess "highlightSortedColumns(): incorrect usage";
     
     #Если есть несколько столбцов, значит есть дефолтный, значит есть активный
     return NG::Block::M_OK unless $self->{_shlistActiveOrder};
@@ -2179,46 +2182,60 @@ sub highlightSortedColumns {
     my $dir   = $self->{_shlistActiveOrder}->{DIR};
     
     my $myurl = $self->q()->url();
-    foreach my $iorder (@{$self->{_orders}}) {
-        #Ищем соответствующее поле
-        my $field = undef;
-        foreach (@{$listfields}) {
-            if ($_->{FIELD} eq $iorder->{FIELD})  {
-                $field = $_;
-                last;
-            }
-        }
-        if (!defined $field) {
-            last if (scalar @{$self->{_orders}} == 1);
-            return $self->error("Некорректная конфигурация модуля: поле сортировки не найдено в списке столбцов");
-        };
     
+    #Формируем хеш всех столбцов, которые можно сортировать
+    my $ordered = {};
+    foreach my $iorder (@{$self->{_orders}}) {
+        $ordered->{$iorder->{FIELD}} = $iorder;
+    };
+    
+    foreach my $cell (@$columns) {
+        next unless $cell->{FIELD};
+        my $iorder = delete $ordered->{$cell->{FIELD}};
+        next unless $iorder; #Сортировка по столбцу отсутствует
+        
         if ($iorder->{FIELD} eq $found->{FIELD}) {
-            #$field->{SELECTED_ORDER} = 1;
-            $field->{"SELECTED_".$dir} = 1;
+            $cell->{"SELECTED_".$dir} = 1;
             if ($dir eq "ASC") {
-                $field->{ORDER} = getURLWithParams($myurl,"desc=".$iorder->{FIELD},$self->getFKParam(),$self->getFilterParam());
+                $cell->{ORDER} = getURLWithParams($myurl,"desc=".$iorder->{FIELD},$self->getFKParam(),$self->getFilterParam());
             } elsif ($dir eq "DESC") {
-                $field->{ORDER} = getURLWithParams($myurl,"asc=".$iorder->{FIELD},$self->getFKParam(),$self->getFilterParam());
+                $cell->{ORDER} = getURLWithParams($myurl,"asc=".$iorder->{FIELD},$self->getFKParam(),$self->getFilterParam());
             };
         }
         else {
             $iorder->{DEFAULTBY} ||= "ASC";
-            $field->{ORDER} = getURLWithParams($myurl,lc($iorder->{DEFAULTBY})."=".$iorder->{FIELD},$self->getFKParam(),$self->getFilterParam());
+            $cell->{ORDER} = getURLWithParams($myurl,lc($iorder->{DEFAULTBY})."=".$iorder->{FIELD},$self->getFKParam(),$self->getFilterParam());
         };
     };
+    
+    if (scalar keys %$ordered) {
+    #    last if (scalar @{$self->{_orders}} == 1);
+        return $self->error("Некорректная конфигурация модуля: поле сортировки ".((keys %$ordered)[0])." не найдено в списке столбцов");
+    };
+    
     return NG::Block::M_OK;
 };
 
-#sub getListHeaders {  ## Prototype only
-#    
-#};
 
 #Методы для компоновки SQL-запроса отображения списка
 sub getListSQLFields {
     my $self = shift;
-    die "Variable \"shlistFields\" is not initialised. Probally you need call getListColumns() method." if !defined $self->{_shlistFields};
-    return $self->{_shlistFields};
+    my $listFObjs = shift or die "getListSQLFields(): incorrect usage";
+    
+    # Составляем строку-список запрашиваемых полей. При этом контролируем вхождение туда ключевого поля.
+    my $fields = "";
+    my $idfound = 0;
+    foreach my $fObj (@$listFObjs) {
+        $idfound = 1 if $fObj->{TYPE} eq "id";
+        my @fieldDbFields = $fObj->dbFields('load');
+        $fields .= ',' if $fields && $fields !~ /\,$/;
+        $fields .= join(",",@fieldDbFields);
+    };
+    if ($idfound == 0) {
+        $fields .= ',' if $fields && $fields !~ /\,$/;
+        $fields .= $self->{_idname}; # Если ключевое поле не найдено
+    };
+    return $fields;
 };
 
 sub getListSQLTable {
@@ -2386,7 +2403,7 @@ sub showSearchForm { #Action!
 	#Если в описании таблицы есть FK, учитываем их в ссылках и SQL (createSearchForm() использует getFKParam() и getFilterParam())
 	$self->processFKFields() or return $self->showError("showSearchForm(): Ошибка вызова processFKFields()");
 	$self->processFilters() or return $self->showError("showSearchForm(): Ошибка вызова processFilters()");
-	$self->processSorting([]) or return $self->showError("showSearchForm(): Ошибка вызова processSorting()");
+	$self->processSorting() or return $self->showError("showSearchForm(): Ошибка вызова processSorting()");
 	
 	my $sform = $self->createSearchForm() or return $self->showError("showSearchForm(): Ошибка вызова createSearchForm()");
 

perl/lib/NG/Admins.pm
@@ -335,6 +335,7 @@ sub buildList {
 	
     $groups = [] unless scalar @$groups > 1;
 	$self->tmpl()->param(
+        SGROUP => $sGroup,
         GROUPS => $groups,
         ADMINS => \@admins,
         URL => $baseUrl,

perl/lib/NG/Field.pm
@@ -44,7 +44,8 @@ our $classesMap = {
    
    OLDDBVALUE - значение, загруженное из БД. Выставляется в setLoadedValue().
    DBVALUE    - значение, загруженное из БД или подготовленное для сохранения в БД.
-   VALUE      - значение, соответствующее текущему значению DBVALUE (ключ не обязательный)
+   VALUE      - значение, соответствующее текущему значению DBVALUE 
+                (ключ не обязательный, однако может использоваться в шаблонах форм, если пользоваться $form->print($tmpl)... Хотя для этого есть sub VALUE {}.)
               - Ключи DBVALUE и VALUE меняются синхронно, в функциях _setValue() и _setDBValue()
    _new       - Признак, что строка новая.
 =cut
@@ -1449,6 +1450,21 @@ sub getListCellHTML {
         $ret .= $url?"</a>":'</p>';
         return $ret;
     }
+};
+
+sub getListHeaderCell {
+    my $field = shift;
+    my ($type) = ($field->{TYPE});
+    
+    my $class = '';
+    $class =  'center'   if $type eq 'checkbox';
+    $class =  'posorder' if $type eq 'posorder';
+    return {
+        NAME  => $field->{NAME},
+        CLASS => $class,
+        TEMPLATE => $field->{HEADER_TEMPLATE}, #BW compat
+        WIDTH    => $field->{WIDTH},
+    };
 };
 
 sub getFieldActions {

perl/lib/NG/Module/List/Row.pm
@@ -32,6 +32,7 @@ sub init {
     $self->{ID_VALUE} = $id;      #В том числе для шаблона
     
     foreach my $field (@{$self->{_listObj}->{_listfields}}) {
+        next if $field->{FIELD} eq "_counter_";
         my $c = NG::Field->new($field,$self) or die $NG::Field::errstr;
         $self->_addColumn($c);
     };
@@ -42,6 +43,11 @@ sub init {
     foreach my $t (@{$self->{COLUMNS}}) {
         $t->{HTML} = $t->{FIELD}->getListCellHTML();
     };
+    if ($self->{_listObj}->{_showCounter}) {
+        unshift @{$self->{COLUMNS}}, {
+            HTML => $self->{_index},
+        };
+    };
 };
 
 #Метод, используемый в унаследованных классах
@@ -51,13 +57,7 @@ sub _addColumn {
     return if ($field->{TYPE} eq "posorder");
     
     $self->{HFIELDS}->{uc($field->{FIELD})} = $field;
-    
-    if ($field->{FIELD} eq "_counter_") {
-        $field->setDBValue($self->number());
-    }
-    else {
-        $field->setDBValue($self->getParam($field->{FIELD}));
-    };
+    $field->setLoadedValue($self->{ROW});
     
     unless ($field->{TYPE} eq "hidden" || $field->{HIDE}) {
         push @{$self->{COLUMNS}}, {

perl/lib/NG/Polls.pm
@@ -20,6 +20,10 @@ sub moduleBlocks {
 };
 
 sub pollsConfig {
+    #Keys:
+    # IMAGEFIELDS    - field configuration hash
+    # IMAGEFORROTATE - Require image when 'rotate' set
+    # DISABLE_HIDERESULT - Disable 'visible' field
     return {};
 };
 
@@ -634,14 +638,14 @@ sub config  {
     my $formfields = $m->formfields();
     my $listfields = $m->listfields();
     
-    my $imageFields = $m->pollsConfig()->{IMAGEFIELDS};
+    my $pollsConfig = $m->pollsConfig();
+    my $imageFields = $pollsConfig->{IMAGEFIELDS};
     
     $self->fields(
         {FIELD=>'id',         TYPE=>'id',     NAME=>'Код записи'},
         {FIELD=>'question',   TYPE=>'text',   NAME=>'Текст вопроса', IS_NOTNULL=>1},
         {FIELD=>'start_date', TYPE=>'date',   NAME=>'Дата начала (дд.мм.гггг)',   IS_NOTNULL=>1,DEFAULT=>current_date()},
         {FIELD=>'end_date',   TYPE=>'date',   NAME=>'Дата окончания (дд.мм.гггг)'},
-        {FIELD=>'visible',    TYPE=>'checkbox', NAME=>'Показывать результаты', IS_NOTNULL=>0},
         {FIELD=>'rotate',     TYPE=>'checkbox', NAME=>'Ротировать',IS_NOTNULL=>0},
         {FIELD=>'check_ip',   TYPE=>'checkbox', NAME=>'Проверять IP',         IS_NOTNULL=>0},
         {FIELD=>'multichoice',TYPE=>'checkbox', NAME=>'Несколько вариантов',  IS_NOTNULL=>0},
@@ -649,6 +653,16 @@ sub config  {
         @$fields
     );
     $self->fields($imageFields) if $imageFields;
+    if ($pollsConfig->{DISABLE_HIDERESULT}) {
+        $self->fields(
+            {FIELD=>'visible',    TYPE=>'hidden',   NAME=>'Показывать результаты', IS_NOTNULL=>0, DEFAULT=>1, HIDE=>1},
+        );
+    }
+    else {
+        $self->fields(
+            {FIELD=>'visible',    TYPE=>'checkbox', NAME=>'Показывать результаты', IS_NOTNULL=>0},
+        );
+    };
     
     # Списковая
     $self->listfields(
@@ -765,13 +779,8 @@ sub deleteAnswer {
 		};
 	}
 	else {
-		if ($is_ajax) {
-			return $self->output("<script>parent.document.getElementById('deleteform1_".$answer_id."').style.display='';parent.document.getElementById('deleteform2_".$answer_id."').style.display='';</script>");
-		}
-		else {
-			$q->param("delete_answer_id",$answer_id);
-			return $self->showOrUpdateAnswers("showanswers",0);
-		};
+		$q->param("delete_answer_id",$answer_id);
+		return $self->showOrUpdateAnswers("showanswers",0);
 	};
 };
 
@@ -840,8 +849,8 @@ sub showOrUpdateAnswers {
 	my @new_answers = ();
 	my $has_errors=0;
     while (my $row=$sth->fetchrow_hashref()) {
-		$row->{VOTE_PRC} = 0;
-        $row->{VOTE_PRC} = sprintf("%.2f",($row->{vote_cnt}*100/$poll->{vote_cnt})) if ($poll->{vote_cnt}>0);
+		$row->{vote_prc} = 0;
+        $row->{vote_prc} = sprintf("%.2f",($row->{vote_cnt}*100/$poll->{vote_cnt})) if ($poll->{vote_cnt}>0);
 		if ($is_update) {
             my $error="";
 			#validation
@@ -858,12 +867,12 @@ sub showOrUpdateAnswers {
 				$def = ($q->param("def") && $q->param("def") == $row->{id})?1:0;
 			}
 			push @new_answers, {
-				ID     => $row->{id},
-				ANSWER => $answer,
-				DEF    => $def,
-                ERROR  => $error,
-                VOTE_CNT => $row->{vote_cnt},
-                VOTE_PRC => $row->{VOTE_PRC},
+				id     => $row->{id},
+				answer => $answer,
+				def    => $def,
+                error  => $error,
+                vote_cnt => $row->{vote_cnt},
+                vote_prc => $row->{vote_prc},
 			};
 		};
 		$row->{for_delete} = ($delete_answer_id == $row->{id})?1:0;

templates/admin-side/admin/admingroup.tmpl
@@ -1,9 +1,8 @@
 <TMPL_IF IS_SELECT>
    <div class="errormsg" id="error_<TMPL_VAR FIELD><TMPL_VAR FORM.KEY_VALUE>"><TMPL_VAR ERRORMSG></div>
-	 <div class="form_contener">
-    <p class="comment_input"><TMPL_VAR NAME></p>
-	<TMPL_IF IS_NOTNULL><p class="comment_simbol">*</p></TMPL_IF>
-	</div>
+    <div class="form_contener">
+    <TMPL_VAR NAME> <TMPL_IF IS_NOTNULL>*</TMPL_IF>
+    </div>
       <div class="input_info">
          <div class="selectWrapper" style="margin-right:5px;"> 
             <select name="<TMPL_VAR FIELD>" id="<TMPL_VAR FIELD><TMPL_VAR FORM.KEY_VALUE>" class="info_text" <TMPL_IF READONLY>DISABLED</TMPL_IF>>

templates/admin-side/admin/adminlist.tmpl
@@ -46,10 +46,10 @@
 
 <div id="info_content">
 		<TMPL_IF GROUPS#>
-		<div style="width:100%; padding-top:15px;">
-        <table id="content" style="padding-top:0px;margin-top:0px;">
+		<div style="width:100%; margin-top:15px; margin-bottom:15px;">
+        <table class="content">
         	<tr>
-                <td class="content">
+                <td class="border">
         			<form action="<TMPL_VAR URL>" method="GET">
                             <div class="selectWrapper" style="margin-right:5px;margin-top:2px;">
 							<select name="group_id">
@@ -79,47 +79,40 @@
 
 
     <div id="table_content">
-		
-		<table id="content" class="admin-table">
-        	
+        <p>Группа: <TMPL_VAR SGROUP.NAME></p>
+		<table class="content">
             <!-- Строка заголовков -->
+            <tr class="title">
+             <td>Логин</td>
+             <td>ФИО</td>
+            </tr>
 
             <!-- /Строка заголовков -->
             
             
-            	<!-- data rows -->
-	    		<TMPL_LOOP ADMINS>
-				<!-- data columns -->
-				<TMPL_IF NEWGROUP>
-				<tr>
-					<td colspan="2" class="title"><p>Группа: <TMPL_VAR GROUP_NAME></p></td>
-				</tr>
-				</TMPL_IF>
-				<TMPL_IF NEWLEVEL>
-				<tr>
-					<td colspan="2" class="title"><p>Уровень: <TMPL_VAR LEVEL></p></td>
-				</tr>
-	            <tr>            
-						<td class="title"><p>Логин</p></td>
-						<td class="title"><p>ФИО</p></td>
-	            </tr>				
-				</TMPL_IF>				
-				<tr >					
-					<td class="content"><TMPL_VAR login></td>
-					<td class="content"><TMPL_VAR fio></td>
-				</tr> 
-				
+                <!-- data rows -->
+                <TMPL_LOOP ADMINS>
+                <!-- data columns -->
+                <TMPL_IF newlevel>
+                <tr class="group_l1">
+                    <td colspan="2"><p>Уровень: <TMPL_VAR level></p></td>
+                </tr>
+                </TMPL_IF newlevel>
+                <tr class="list">
+                    <td><TMPL_VAR login></td>
+                    <td><TMPL_VAR fio></td>
+                </tr> 
+                
                 <!-- /Строка элементов управления записи -->
-                <tr>
-                 
-                    <td class="last_line_right_img" colspan="2">
+                <tr class="actions">
+                    <td colspan="2">
                         <TMPL_LOOP EXTRA_LINKS>
 						<nobr>
-                            <a href="<TMPL_VAR URL>" <TMPL_IF AJAX_URL>onclick="ajax_url('<TMPL_VAR AJAX_URL>','admin_<TMPL_VAR ID>');return false;"</TMPL_IF AJAX_URL>><TMPL_VAR NAME></a>
+                            <a href="<TMPL_VAR URL>" <TMPL_IF AJAX_URL>onclick="ajax_url('<TMPL_VAR AJAX_URL>','admin_<TMPL_VAR id>');return false;"</TMPL_IF AJAX_URL>><TMPL_VAR NAME></a>
 							&nbsp;|&nbsp;
                         </TMPL_LOOP>
-                        <TMPL_IF EDIT_URL><a href="<TMPL_VAR EDIT_URL>" <TMPL_IF AJAX_EDIT_URL>onclick="ajax_url('<TMPL_VAR AJAX_EDIT_URL>','admin_<TMPL_VAR ID>');return false;"</TMPL_IF>>Редактировать</a>&nbsp;|&nbsp;</TMPL_IF EDIT_URL>
-                        <TMPL_IF DELETE_URL><a href="<TMPL_VAR DELETE_URL>" <TMPL_IF AJAX_DELETE_URL>onclick="ajax_url('<TMPL_VAR AJAX_DELETE_URL>','admin_<TMPL_VAR ID>');return false;"</TMPL_IF>>Удалить</a></TMPL_IF DELETE_URL>
+                        <TMPL_IF EDIT_URL><a href="<TMPL_VAR EDIT_URL>" <TMPL_IF AJAX_EDIT_URL>onclick="ajax_url('<TMPL_VAR AJAX_EDIT_URL>','admin_<TMPL_VAR id>');return false;"</TMPL_IF>>Редактировать</a>&nbsp;|&nbsp;</TMPL_IF EDIT_URL>
+                        <TMPL_IF DELETE_URL><a href="<TMPL_VAR DELETE_URL>" <TMPL_IF AJAX_DELETE_URL>onclick="ajax_url('<TMPL_VAR AJAX_DELETE_URL>','admin_<TMPL_VAR id>');return false;"</TMPL_IF>>Удалить</a></TMPL_IF DELETE_URL>
     					</nobr>	             
 				    </td>
 					
@@ -127,7 +120,7 @@
                 <!-- /Строка элементов управления записи -->
 
 				<!-- form container -->
-	        	<tr><td id="admin_<TMPL_VAR ID>" colspan="2" style="padding:0px;border:none;"></td></tr>				
+	        	<tr><td class="formcontainer" id="admin_<TMPL_VAR id>" colspan="2"></td></tr>				
 	        	<!-- form container / -->
 				
 	    	</TMPL_LOOP>			

commit 90ce686933909caa8689875995f2076487b9a631

* Fix bugs introduced by r2435
git-svn-id: http://svn-proxy/repos/ng/branches/ng6@2439 5d2ed3fb-774c-4d2d-a2ad-e8474e2e3017

perl/lib/NG/Module/List.pm
@@ -2150,6 +2150,7 @@ sub getListHeaders {
     my @headers = ();
     foreach my $fObj (@{$listFObjs}) {
         next if $fObj->{TYPE} eq "posorder";
+        next if $fObj->{TYPE} eq "hidden";
         #
         my $headerCell = $fObj->getListHeaderCell();
         $headerCell->{FIELD} = $fObj->{FIELD};
@@ -2208,8 +2209,7 @@ sub highlightSortedColumns { #Подсветка столбцов, по которым можно делать сортир
         };
     };
     
-    if (scalar keys %$ordered) {
-    #    last if (scalar @{$self->{_orders}} == 1);
+    if (scalar keys %$ordered > 1) {
         return $self->error("Некорректная конфигурация модуля: поле сортировки ".((keys %$ordered)[0])." не найдено в списке столбцов");
     };
     