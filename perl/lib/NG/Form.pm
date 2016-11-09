53de4888b6edace8dd19efdfe658a88de6866198
Pavel Rochnyack <pavel2000@ngs.ru>
2016-11-09 21:22:50 +0000

    Reverted r2600
    git-svn-id: http://svn-proxy/repos/ng/branches/ng6@2601 5d2ed3fb-774c-4d2d-a2ad-e8474e2e3017

--- perl/lib/NG/Form.pm
+++ perl/lib/NG/Form.pm
@@ -501,7 +501,6 @@ sub print {
                         delete $fHash->{$fn};
                         
                         my $f = $field->param();
-                        $f->{'FORM:ROW_WITH_COLUMNS'}=1;
                         push @elements, $f;
                     };
                     push @elements, {COLUMN_END=>1,COLUMN=>$c};
